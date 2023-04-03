062fb5dbcf0e969acc27e343b98145677919324d
Streetmeat25 <kotpersikchannel@gmail.com>
2023-04-03 16:56:42 +1000

    image folder

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,42 +8,42 @@ export class DataService {
 
   private data: any = [
     {
-      image: "assets/pizzas/Layer 2.png",
+      image: "assets/images/pizzas/Layer 2.png",
       title: "Мясная Делюкс",
       description: "Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы"
     },
     {
-      image: "assets/pizzas/Layer 3.png",
+      image: "assets/images/pizzas/Layer 3.png",
       title: "Морская Премиум",
       description: "Перец, сыр, креветки, кальмары, мидии, лосось"
     },
     {
-      image: "assets/pizzas/Layer 4.png",
+      image: "assets/images/pizzas/Layer 4.png",
       title: "Бекон и Сосиски",
       description: "Бекон, сыр, сосиски, ананас, томатная паста"
     },
     {
-      image: "assets/pizzas/Layer 5.png",
+      image: "assets/images/pizzas/Layer 5.png",
       title: "Куриная Делюкс",
       description: "Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста"
     },
     {
-      image: "assets/pizzas/Layer 6.png",
+      image: "assets/images/pizzas/Layer 6.png",
       title: "Барбекю Премиум",
       description: "Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили"
     },
     {
-      image: "assets/pizzas/Layer 7.png",
+      image: "assets/images/pizzas/Layer 7.png",
       title: "Пепперони Дабл",
       description: "Пепперони, сыр, колбаса 2 видов: обжаренная и вареная"
     },
     {
-      image: "assets/pizzas/Layer 8.png",
+      image: "assets/images/pizzas/Layer 8.png",
       title: "Куриное трио",
       description: "Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы"
     },
     {
-      image: "assets/pizzas/Layer 9.png",
+      image: "assets/images/pizzas/Layer 9.png",
       title: "Сырная",
       description: "Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный"
     }
